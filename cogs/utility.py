905d5625f26b3aeaac8fac286c30d26a04c60ace
user5020250 <user5020250@gmail.com>
2026-08-02 01:10:22 +0800

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -41,6 +41,19 @@ class ReactionRoleView(discord.ui.View):
 
 class Utility(commands.Cog):
 
+    say_group = app_commands.Group(
+        name="say",
+        description="Send or edit a message as the bot."
+    )
+    embed_group = app_commands.Group(
+        name="embed",
+        description="Send or edit a custom embed."
+    )
+    sticky_group = app_commands.Group(
+        name="sticky",
+        description="Create or edit a sticky message in this channel."
+    )
+
     def __init__(self, bot):
         self.bot = bot
         self.sticky_messages = {}
@@ -49,8 +62,8 @@ class Utility(commands.Cog):
     # SAY
     # --------------------
 
-    @app_commands.command(
-        name="say",
+    @say_group.command(
+        name="send",
         description="Sends a message as the bot."
     )
     @app_commands.describe(
@@ -60,7 +73,7 @@ class Utility(commands.Cog):
         img="Optional image URL (sent as an embed if provided)."
     )
     @app_commands.checks.has_permissions(manage_messages=True)
-    async def say(
+    async def say_send(
         self,
         interaction: discord.Interaction,
         description: str,
@@ -94,12 +107,77 @@ class Utility(commands.Cog):
             await interaction.channel.send(description)
 
 
+    @say_group.command(
+        name="edit",
+        description="Edits a message previously sent by the bot."
+    )
+    @app_commands.describe(
+        message_id="The ID of the message to edit.",
+        description="The new message content (required).",
+        title="Optional title (sent as an embed if provided).",
+        footer="Optional footer text (sent as an embed if provided).",
+        img="Optional image URL (sent as an embed if provided)."
+    )
+    @app_commands.checks.has_permissions(manage_messages=True)
+    async def say_edit(
+        self,
+        interaction: discord.Interaction,
+        message_id: str,
+        description: str,
+        title: str = None,
+        footer: str = None,
+        img: str = None
+    ):
+
+        try:
+            msg = await interaction.channel.fetch_message(
+                int(message_id)
+            )
+        except:
+            await interaction.response.send_message(
+                "Invalid message ID.",
+                ephemeral=True
+            )
+            return
+
+        if msg.author.id != self.bot.user.id:
+            await interaction.response.send_message(
+                "I can only edit my own messages.",
+                ephemeral=True
+            )
+            return
+
+        if title or footer or img:
+            embed = discord.Embed(
+                description=description,
+                color=BLACK
+            )
+
+            if title:
+                embed.title = title
+
+            if footer:
+                embed.set_footer(text=footer)
+
+            if img:
+                embed.set_image(url=img)
+
+            await msg.edit(content=None, embed=embed)
+        else:
+            await msg.edit(content=description, embed=None)
+
+        await interaction.response.send_message(
+            "Message edited.",
+            ephemeral=True
+        )
+
+
     # --------------------
     # EMBED
     # --------------------
 
-    @app_commands.command(
-        name="embed",
+    @embed_group.command(
+        name="send",
         description="Sends a custom embed."
     )
     @app_commands.describe(
@@ -109,7 +187,7 @@ class Utility(commands.Cog):
         img="Optional image URL."
     )
     @app_commands.checks.has_permissions(manage_messages=True)
-    async def embed(
+    async def embed_send(
         self,
         interaction: discord.Interaction,
         description: str,
@@ -138,6 +216,68 @@ class Utility(commands.Cog):
         )
 
         await interaction.channel.send(embed=embed)
+
+
+    @embed_group.command(
+        name="edit",
+        description="Edits an embed previously sent by the bot."
+    )
+    @app_commands.describe(
+        message_id="The ID of the embed message to edit.",
+        description="The new embed description (required).",
+        title="Optional title.",
+        footer="Optional footer text.",
+        img="Optional image URL."
+    )
+    @app_commands.checks.has_permissions(manage_messages=True)
+    async def embed_edit(
+        self,
+        interaction: discord.Interaction,
+        message_id: str,
+        description: str,
+        title: str = None,
+        footer: str = None,
+        img: str = None
+    ):
+
+        try:
+            msg = await interaction.channel.fetch_message(
+                int(message_id)
+            )
+        except:
+            await interaction.response.send_message(
+                "Invalid message ID.",
+                ephemeral=True
+            )
+            return
+
+        if msg.author.id != self.bot.user.id:
+            await interaction.response.send_message(
+                "I can only edit my own messages.",
+                ephemeral=True
+            )
+            return
+
+        embed = discord.Embed(
+            description=description,
+            color=BLACK
+        )
+
+        if title:
+            embed.title = title
+
+        if footer:
+            embed.set_footer(text=footer)
+
+        if img:
+            embed.set_image(url=img)
+
+        await msg.edit(embed=embed)
+
+        await interaction.response.send_message(
+            "Embed edited.",
+            ephemeral=True
+        )
 
 
     # --------------------
@@ -275,8 +415,8 @@ class Utility(commands.Cog):
     # STICKY
     # --------------------
 
-    @app_commands.command(
-        name="sticky",
+    @sticky_group.command(
+        name="set",
         description="Creates a sticky message in this channel."
     )
     @app_commands.describe(
@@ -291,7 +431,7 @@ class Utility(commands.Cog):
         app_commands.Choice(name="embed", value="embed"),
     ])
     @app_commands.checks.has_permissions(manage_messages=True)
-    async def sticky(
+    async def sticky_set(
         self,
         interaction: discord.Interaction,
         type: app_commands.Choice[str],
@@ -340,6 +480,93 @@ class Utility(commands.Cog):
         )
 
         await interaction.channel.send(embed=embed)
+
+
+    @sticky_group.command(
+        name="edit",
+        description="Edits an existing sticky message."
+    )
+    @app_commands.describe(
+        message_id="The ID of the sticky message to edit.",
+        type="Plain text message or a styled embed.",
+        description="The new sticky text (required).",
+        title="Optional title (embed only).",
+        footer="Optional footer text (embed only).",
+        img="Optional image URL (embed only)."
+    )
+    @app_commands.choices(type=[
+        app_commands.Choice(name="message", value="message"),
+        app_commands.Choice(name="embed", value="embed"),
+    ])
+    @app_commands.checks.has_permissions(manage_messages=True)
+    async def sticky_edit(
+        self,
+        interaction: discord.Interaction,
+        message_id: str,
+        type: app_commands.Choice[str],
+        description: str,
+        title: str = None,
+        footer: str = None,
+        img: str = None
+    ):
+
+        try:
+            msg = await interaction.channel.fetch_message(
+                int(message_id)
+            )
+        except:
+            await interaction.response.send_message(
+                "Invalid message ID.",
+                ephemeral=True
+            )
+            return
+
+        if msg.author.id != self.bot.user.id:
+            await interaction.response.send_message(
+                "I can only edit my own messages.",
+                ephemeral=True
+            )
+            return
+
+        if type.value == "message":
+            await msg.edit(content=description, embed=None)
+
+            self.sticky_messages[interaction.channel.id] = {
+                "type": "text",
+                "content": description,
+            }
+
+            await interaction.response.send_message(
+                "Sticky message edited.",
+                ephemeral=True
+            )
+            return
+
+        embed = discord.Embed(
+            description=description,
+            color=BLACK
+        )
+
+        if title:
+            embed.title = title
+
+        if footer:
+            embed.set_footer(text=footer)
+
+        if img:
+            embed.set_image(url=img)
+
+        await msg.edit(content=None, embed=embed)
+
+        self.sticky_messages[interaction.channel.id] = {
+            "type": "embed",
+            "embed": embed,
+        }
+
+        await interaction.response.send_message(
+            "Sticky message edited.",
+            ephemeral=True
+        )
 
 
     @commands.Cog.listener()
